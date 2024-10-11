812a8ec4b8f13b52d28c71e5ea9dac094a3766fa
Balazs Simon <balazssimon@gmail.com>
2024-10-11 22:46:33 +0200

    WebTest metamodel registration as a resource

--- webtest.dsl.parent/webtest.dsl/src/webtest/dsl/WebTestDslStandaloneSetup.java
+++ webtest.dsl.parent/webtest.dsl/src/webtest/dsl/WebTestDslStandaloneSetup.java
@@ -3,6 +3,11 @@
  */
 package webtest.dsl;
 
+import org.eclipse.emf.ecore.EPackage;
+
+import com.google.inject.Injector;
+
+import webtest.model.ModelPackage;
 
 /**
  * Initialization support for running Xtext languages without Equinox extension registry.
@@ -11,5 +16,13 @@ public class WebTestDslStandaloneSetup extends WebTestDslStandaloneSetupGenerate
 
 	public static void doSetup() {
 		new WebTestDslStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+	
+	@Override
+	public void register(Injector injector) {
+		if (!EPackage.Registry.INSTANCE.containsKey(ModelPackage.eNS_URI)) {
+            EPackage.Registry.INSTANCE.put(ModelPackage.eNS_URI, ModelPackage.eINSTANCE);
+        }
+		super.register(injector);
 	}
 }
